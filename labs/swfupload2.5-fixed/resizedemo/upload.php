8bfeaa4f044234d6fe2ff9a1a8c2a60edce9a2d2
ranklau <ranklau@gmail.com>
2012-09-12 14:41:05 +0800

    upload component of swfupload2.5 fixed by rank

---
+++ labs/swfupload2.5-fixed/resizedemo/upload.php
@@ -0,0 +1,33 @@
+
+<?php
+	/* Note: This thumbnail creation script requires the GD PHP Extension.  
+		If GD is not installed correctly PHP does not render this page correctly
+		and SWFUpload will get "stuck" never calling uploadSuccess or uploadError
+	 */
+
+	// Get the session Id passed from SWFUpload. We have to do this to work-around the Flash Player Cookie Bug
+	if (isset($_POST["PHPSESSID"])) {
+		session_id($_POST["PHPSESSID"]);
+	}
+
+	session_start();
+	ini_set("html_errors", "0");
+
+	// Check the upload
+	if (!isset($_FILES["Filedata"])) {
+		echo "ERROR:invalid upload";
+		exit(0);
+	}
+
+	if (!isset($_SESSION["file_info"])) {
+		$_SESSION["file_info"] = array();
+	}
+
+	$file_id = md5(rand()*10000000);
+	
+	$_SESSION["file_info"][$file_id] = file_get_contents($_FILES['Filedata']['tmp_name']);
+
+	echo "FILEID:" . $file_id;	// Return the file id to the script
+	exit(0);
+	
+?>
